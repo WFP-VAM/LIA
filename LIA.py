2843f48a56ebd522e4548d874f79613882134be1
trenouard <theo.renouard@hotmail.com>
2022-04-06 11:42:14 +0200

    Update expansion_ndvi.py

--- LIA.py
+++ LIA.py
@@ -1,52 +1,56 @@
 from helper_fns import *
 import pre_post
+import expansion_ndvi
 
 def main():
 
-	# Set paths
-	path_to_shapefile = 'data/Shapefiles/'
-	path_to_asset_info = 'data/Dataframes/asset_info.csv'
-	path_to_country_info = 'data/Dataframes/country_info.csv'
-	path_to_ODC_url = 'data/Rasters/ODC_url.csv'
+    # Set paths
+    path_to_shapefile = 'data/Shapefiles/'
+    path_to_asset_info = 'data/Dataframes/asset_info.csv'
+    path_to_country_info = 'data/Dataframes/country_info.csv'
+    path_to_ODC_url = 'data/Rasters/ODC_url.csv'
 
-	# Delete outputs
-	path_output = 'outputs'
-	delete_directory(path_output)
-
-
-	# DOWNLOAD DATA
-
-	# Shapefiles
-	shapefiles = glob.glob(path_to_shapefile + '*.shp')
-
-	# CSV
-	asset_info = read_asset_csv(path_to_asset_info)
-	country_info = pd.read_csv(path_to_country_info, index_col = 1, header = 0)
-
-	# Rasters
-	ODC_url = pd.read_csv(path_to_ODC_url, index_col = 0, header = None)
-	ODC_url.rename(columns = {1: 'url'}, inplace = True)
-
-	fs = s3fs.S3FileSystem(anon=True, client_kwargs={'region_name': 'eu-central-1'})
-	NDVI = xr.open_zarr(s3fs.S3Map(ODC_url.loc['NDVI']['url'], s3=fs))
-	NDVI = NDVI.assign_coords(time = pd.to_datetime([string_to_date(x) for x in NDVI.time.values]))
-	NDVI = NDVI.band
+    # Delete outputs
+    path_output = 'outputs'
+    if os.path.exists(path_output):
+        delete_directory(path_output)
 
 
-	# Get Country information 
-	iso3 = os.path.basename(shapefiles[0])[:3]
-	c_info = country_info.loc[iso3]
-	(wet_season, dry_season) = get_wet_dry(c_info)
+    # DOWNLOAD DATA
+
+    # Shapefiles
+    shapefiles = glob.glob(path_to_shapefile + '*.shp')
+
+    # CSV
+    asset_info = read_asset_csv(path_to_asset_info)
+    country_info = pd.read_csv(path_to_country_info, index_col = 1, header = 0, encoding='ISO-8859-1')
+
+    # Rasters
+    ODC_url = pd.read_csv(path_to_ODC_url, index_col = 0, header = None)
+    ODC_url.rename(columns = {1: 'url'}, inplace = True)
+
+    fs = s3fs.S3FileSystem(anon=True, client_kwargs={'region_name': 'eu-central-1'})
+    NDVI = xr.open_zarr(s3fs.S3Map(ODC_url.loc['NDVI']['url'], s3=fs))
+    NDVI = NDVI.assign_coords(time = pd.to_datetime([string_to_date(x) for x in NDVI.time.values]))
+    NDVI = NDVI.band
 
 
-	# PROCESSING
+    # Get Country information 
+    iso3 = os.path.basename(shapefiles[0])[:3]
+    c_info = country_info.loc[iso3]
+    (wet_season, dry_season) = get_wet_dry(c_info)
 
-	pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'NDVI')
+
+    # PROCESSING
+
+    #pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'NDVI')
+    
+    expansion_ndvi.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
 
 
 if __name__ == '__main__':
 
-	main()
+    main()
 
 
 
--- expansion_ndvi.py
+++ expansion_ndvi.py
@@ -1,8 +1,12 @@
 # regional average monthly rainfall and max NDVI values during year, irrigation crops
 
-import csv
+import os
 import pathlib
 import pandas as pd
+import geopandas as gpd
+from datetime import date
+from matplotlib import colors
+import matplotlib.pyplot as plt
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -61,60 +65,88 @@ def date_to_str(x: tuple):
 def run(da, shapefiles: list, wet_season: list, dry_season: list, asset_info: pd.DataFrame, path_output: str):
 
     # Create output folder
-	folder_name = path_output + '/' + 'NDVI' + '_expansion'
-	pathlib.Path(folder_name).mkdir(parents=True, exist_ok=True)
+    folder_name = path_output + '/' + 'NDVI' + '_expansion'
+    pathlib.Path(folder_name).mkdir(parents=True, exist_ok=True)
     
     for i,shapefile in enumerate(shapefiles):
-	    
-		# Get asset ID
-		ID = os.path.basename(shapefile)[:-4]
-		print('-- Processing asset ' + ID + ' (' + str(i + 1) + '/' + str(len(shapefiles)) + ') --')
+        
+        # Get asset ID
+        ID = os.path.basename(shapefile)[:-4]
+        print('-- Processing asset ' + ID + ' (' + str(i + 1) + '/' + str(len(shapefiles)) + ') --')
 
-		# Reading asset
-		gdf = gpd.read_file(shapefile)
+        # Reading asset
+        gdf = gpd.read_file(shapefile)
 
-		# 0.5 degree buffer around asset
-		gdf_buf = gdf.buffer(0.2, cap_style = 3)
+        # Clip rasters
+        da_clipped = da.rio.clip(gdf.geometry.values, gdf.crs)
 
-		# Clip rasters
-        da_clipped = da_ndvi.rio.clip(gdf.geometry.values, gdf.crs)
-
-		# Load da values
+        # Load da values
         da_clipped.load()
 
-		# Get intervention dates
-		start_intervention = asset_info.loc[ID].start
-		end_intervention = asset_info.loc[ID].end
+        # Get intervention dates
+        start_intervention = asset_info.loc[ID].start
+        end_intervention = asset_info.loc[ID].end
 
-		# Get comparison dates
-		(pre_wet, post_wet, pre_dry, post_dry) = get_pre_post_dates(start_intervention, end_intervention, wet_season, dry_season)
+        # Get comparison dates
+        (pre_wet, post_wet, pre_dry, post_dry) = get_pre_post_dates(start_intervention, end_intervention, wet_season, dry_season)
 
         t = da_clipped.time.values
         
         for prew, postw in zip(pre_wet, post_wet):
-		    
-		    pre = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(prew[0][1], prew[0][0], 1))) & (t <= pd.to_datetime(date(prew[1][1], prew[1][0], 1)))])
-		    post = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(postw[0][1], postw[0][0], 1))) & (t <= pd.to_datetime(date(postw[1][1], postw[1][0], 1)))])
-		    
-            exp_pre = pre.where(pre.band >= 0.5)
-            print(exp_pre)
-            exp_post = pre.where(pre.band >= 0.5)
-        
-		    
-		    name_pre = ID + '_L_' + 'NDVI' + '_' + date_to_str(prew[0]) + '_' + date_to_str(prew[1]) + '_wet.tif'
-		    exp_pre.rio.to_raster(folder_name + '/' + name_pre)
-            ######### Modify above #############"
-            name_post = 
-		    
-		    
-		for pred, postd in zip(pre_dry, post_dry):
-		    
-		    pre = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(pred[0][1], pred[0][0], 1))) & (t <= pd.to_datetime(date(pred[1][1], pred[1][0], 1)))])
-		    post = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(postd[0][1], postd[0][0], 1))) & (t <= pd.to_datetime(date(postd[1][1], postd[1][0], 1)))])
-		    diff = post.mean(['time']) - pre.mean(['time'])
+            
+            pre = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(prew[0][1], prew[0][0], 1))) & (t <= pd.to_datetime(date(prew[1][1], prew[1][0], 1)))]).max(dim='time')
+            post = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(postw[0][1], postw[0][0], 1))) & (t <= pd.to_datetime(date(postw[1][1], postw[1][0], 1)))]).max(dim='time')
+            
+            exp_pre = pre.where(pre >= 0.5)
+            exp_post = post.where(post < 0.5)       
 
-		    name = ID + '_L_' + product_type + '_' + date_to_str(pred[0]) + '_' + date_to_str(pred[1]) + '_' + date_to_str(postd[0]) + '_' + date_to_str(postd[1]) + '_dry.tif'
-		    diff.rio.to_raster(folder_name + '/' + name)
+            name_pre = ID + '_L_' + 'NDVI' + '_' + date_to_str(prew[0]) + '_' + date_to_str(prew[1]) + '_wet.tif'
+            exp_pre.rio.to_raster(folder_name + '/' + name_pre)
+            name_post = ID + '_L_' + 'NDVI' + '_' + date_to_str(postw[0]) + '_' + date_to_str(postw[1]) + '_wet.tif'
+            exp_post.rio.to_raster(folder_name + '/' + name_post)
+            
+            ### Add plot on a basemap
+            ### Add font title on the image
+            ### Add legend on the image
+            fig, axs  = plt.subplots(1,2,figsize=(10,5))
+            cmap = colors.ListedColormap(['saddlebrown', 'forestgreen'])
+            axs[0].imshow(exp_post, cmap=cmap)
+            axs[0].set_title(date_to_str(postw[0]))
+            
+            axs[1].imshow(exp_pre, cmap=cmap)
+            axs[1].set_title(date_to_str(prew[0]))
+            
+            fig.suptitle('Maximum NDVI for the wet period months to define over ')
+            
+               
+        for pred, postd in zip(pre_dry, post_dry):
+    
+            pre = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(pred[0][1], pred[0][0], 1))) & (t <= pd.to_datetime(date(pred[1][1], pred[1][0], 1)))]).max(dim='time')
+            post = da_clipped.sel(time = da_clipped.time[(t >= pd.to_datetime(date(postd[0][1], postd[0][0], 1))) & (t <= pd.to_datetime(date(postd[1][1], postd[1][0], 1)))]).max(dim='time')
+   
+            exp_pre = pre.where(pre >= 0.5)
+            exp_post = post.where(post < 0.5)        
+    
+            name_pre = ID + '_L_' + 'NDVI' + '_' + date_to_str(pred[0]) + '_' + date_to_str(pred[1]) + '_dry.tif'
+            exp_pre.rio.to_raster(folder_name + '/' + name_pre)
+            name_post = ID + '_L_' + 'NDVI' + '_' + date_to_str(postd[0]) + '_' + date_to_str(postd[1]) + '_dry.tif'
+            exp_post.rio.to_raster(folder_name + '/' + name_post)
+            
+            ### Add plot on a basemap
+            ### Add font title on the image
+            ### Add legend on the image
+            fig, axs  = plt.subplots(1,2,figsize=(10,5))
+            cmap = colors.ListedColormap(['saddlebrown', 'forestgreen'])
+            axs[0].imshow(exp_post, cmap=cmap)
+            axs[0].set_title(date_to_str(postd[0]))
+            
+            axs[1].imshow(exp_pre, cmap=cmap)
+            axs[1].set_title(date_to_str(pred[0]))
+            
+            fig.suptitle('Maximum NDVI for the dry period months to define over ')
+            
+            
+            
 
         
         
