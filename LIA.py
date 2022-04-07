367501c5340784de578eed49a4adfc65790474a3
AmineBarkaoui <amine.barkaoui1@gmail.com>
2022-04-07 18:36:57 +0200

    Include rfh_ndvi

--- LIA.py
+++ LIA.py
@@ -1,5 +1,6 @@
 from helper_fns import *
 import pre_post
+import rfh_ndvi
 
 def main():
 
@@ -10,9 +11,8 @@ def main():
 	path_to_ODC_url = 'data/Rasters/ODC_url.csv'
 
 	# Delete outputs
-	path_output = 'outputs'
+	path_output = './outputs'
 	delete_directory(path_output)
-
 
 	# DOWNLOAD DATA
 
@@ -21,17 +21,22 @@ def main():
 
 	# CSV
 	asset_info = read_asset_csv(path_to_asset_info)
-	country_info = pd.read_csv(path_to_country_info, index_col = 1, header = 0)
+	country_info = pd.read_csv(path_to_country_info, index_col = 1, header = 0, encoding='ISO-8859-1')
 
-	# Rasters
+
+	# Rasters - NDVI
 	ODC_url = pd.read_csv(path_to_ODC_url, index_col = 0, header = None)
 	ODC_url.rename(columns = {1: 'url'}, inplace = True)
-
 	fs = s3fs.S3FileSystem(anon=True, client_kwargs={'region_name': 'eu-central-1'})
+    
 	NDVI = xr.open_zarr(s3fs.S3Map(ODC_url.loc['NDVI']['url'], s3=fs))
 	NDVI = NDVI.assign_coords(time = pd.to_datetime([string_to_date(x) for x in NDVI.time.values]))
 	NDVI = NDVI.band
-
+    
+	CHIRPS = xr.open_zarr(s3fs.S3Map(ODC_url.loc['CHIRPS']['url'], s3=fs))
+	CHIRPS = CHIRPS.assign_coords(time = pd.to_datetime([string_to_date(x) for x in CHIRPS.time.values]))
+	CHIRPS = CHIRPS.band
+    
 
 	# Get Country information 
 	iso3 = os.path.basename(shapefiles[0])[:3]
@@ -41,7 +46,10 @@ def main():
 
 	# PROCESSING
 
-	pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'NDVI')
+	#pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'NDVI')
+    
+	rfh_ndvi.run(CHIRPS, NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
+
 
 
 if __name__ == '__main__':
