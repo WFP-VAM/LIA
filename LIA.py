015965a248185f34c33a2f8bc2b6868880624c4d
AmineBarkaoui <amine.barkaoui1@gmail.com>
2022-04-12 16:40:15 +0200

    Update LIA after merge

--- LIA.py
+++ LIA.py
@@ -24,12 +24,12 @@ def main():
 	asset_info = read_asset_csv(path_to_asset_info)
 	country_info = pd.read_csv(path_to_country_info, index_col = 1, header = 0, encoding='ISO-8859-1')
 
-
 	# Rasters - NDVI
 	ODC_url = pd.read_csv(path_to_ODC_url, index_col = 0, header = None)
 	ODC_url.rename(columns = {1: 'url'}, inplace = True)
 	NDVI = read_ODC(ODC_url.loc['NDVI']['url'])
 	LST = read_ODC(ODC_url.loc['LST']['url'])
+	CHIRPS = read_ODC(ODC_url.loc['CHIRPS']['url'])
 
     # Get Country information 
     iso3 = os.path.basename(shapefiles[0])[:3]
@@ -44,13 +44,10 @@ def main():
 	pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'maxNDVI')
 	print(' ## LST pre/post implementation ##')
 	pre_post.run(LST, shapefiles, wet_season, dry_season, asset_info, path_output, 'LST')
-    # PROCESSING
-
-	#pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'NDVI')
-    
+	print(' ## Rainfall & max NDVI ##')
 	rfh_ndvi.run(CHIRPS, NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
-    
-    #expansion_ndvi.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
+	print(' ## Expansion NDVI ##')
+	expansion_ndvi.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
 
 
 if __name__ == '__main__':
