ced78b3830d5a204cbd1fb667e62585ca54e49d6
JuHolland <33092664+JuHolland@users.noreply.github.com>
2022-04-27 15:38:05 +0200

    Flags

--- LIA.py
+++ LIA.py
@@ -1,60 +1,118 @@
-import pre_post
-import rfh_ndvi
-import expansion_ndvi
-import enso
+import argparse
+from tkinter import *
+
 from helper_fns import *
+import pre_post
+import tci_vci_vhi
 
-def main():
+def main(select: bool):
 
-    # Set paths
-    path_to_shapefile = 'data/Shapefiles/'
-    path_to_asset_info = 'data/Dataframes/asset_info.csv'
-    path_to_country_info = 'data/Dataframes/country_info.csv'
-    path_to_enso = 'data/Dataframes/ENSO.csv'
-    path_to_ODC_url = 'data/Rasters/ODC_url.csv'
+	# Checkbox
+	if select:
 
-	# Delete outputs
-    path_output = './outputs'
-    delete_directory(path_output)
+		a = Tk()
+		a.title('Select')
+
+		positionRight = int(a.winfo_screenwidth()/2 - a.winfo_reqwidth()/2)
+		positionDown = int(a.winfo_screenheight()/2 - a.winfo_reqheight()/2)
+		a.geometry("+{}+{}".format(positionRight, positionDown))
+
+		var1 = IntVar()
+		Checkbutton(a, text = "NDVI pre/post implementation", variable = var1).grid(row = 0, sticky = W)
+		var2 = IntVar()
+		Checkbutton(a, text = "max NDVI pre/post implementation", variable = var2).grid(row = 1, sticky = W)
+		var3 = IntVar()
+		Checkbutton(a, text = "LST pre/post implementation", variable = var3).grid(row = 2, sticky = W)
+		var4 = IntVar()
+		Checkbutton(a, text = "TCI/VCI/VHI pre/post implementation", variable = var4).grid(row = 3, sticky = W)
+		var5 = IntVar()
+		Checkbutton(a, text = "Rainfall and max NDVI", variable = var5).grid(row = 4, sticky = W)
+		var6 = IntVar()
+		Checkbutton(a, text = "Expansion NDVI", variable = var6).grid(row = 5, sticky = W)
+		var7 = IntVar()
+		Checkbutton(a, text = "ENSO analysis", variable = var7).grid(row = 6, sticky = W)
+
+		Button(a, text = 'Run', command = a.destroy).grid(row = 7)
+		a.mainloop()
+
+		run = [var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get()]
+
+	else:
+
+		run = [1, 1, 1, 1, 1, 1, 1]
+
+
+
+	# Set paths
+	path_to_shapefile = 'data/Shapefiles/'
+	path_to_asset_info = 'data/Dataframes/asset_info.csv'
+	path_to_country_info = 'data/Dataframes/country_info.csv'
+	path_to_enso = 'data/Dataframes/ENSO.csv'
+	path_to_ODC_url = 'data/Rasters/ODC_url.csv'
+	path_output = 'outputs'
+
 
 	# DOWNLOAD DATA
 
-    # Shapefiles
-    shapefiles = glob.glob(path_to_shapefile + '*.shp')
+	# Shapefiles
+	shapefiles = glob.glob(path_to_shapefile + '*.shp')
 
 	# CSV
-    asset_info = read_asset_csv(path_to_asset_info)
-    country_info = pd.read_csv(path_to_country_info, index_col = 1, header = 0, encoding='ISO-8859-1')
-    ENSO = pd.read_csv(path_to_enso, index_col = 0, header = 0, sep = ';', encoding='utf8')
+	asset_info = read_asset_csv(path_to_asset_info)
+	country_info = pd.read_csv(path_to_country_info, index_col = 1, header = 0)
+	ENSO = pd.read_csv(path_to_enso, index_col = 0, header = 0, sep = ';', encoding='utf8')
 
-	# Rasters - NDVI
-    ODC_url = pd.read_csv(path_to_ODC_url, index_col = 0, header = None)
-    ODC_url.rename(columns = {1: 'url'}, inplace = True)
-    NDVI = read_ODC(ODC_url.loc['NDVI']['url'])
-    LST = read_ODC(ODC_url.loc['LST']['url'])
-    CHIRPS = read_ODC(ODC_url.loc['CHIRPS']['url'])
+	# Rasters
+	ODC_url = pd.read_csv(path_to_ODC_url, index_col = 0, header = None)
+	ODC_url.rename(columns = {1: 'url'}, inplace = True)
+	NDVI = read_ODC(ODC_url.loc['NDVI']['url'])
+	LST = read_ODC(ODC_url.loc['LST']['url'])
+	CHIRPS = read_ODC(ODC_url.loc['CHIRPS']['url'])
 
-    # Get Country information
-    iso3 = os.path.basename(shapefiles[0])[:3]
-    c_info = country_info.loc[iso3]
-    (wet_season, dry_season) = get_wet_dry(c_info)
+
+	# Get Country information 
+	iso3 = os.path.basename(shapefiles[0])[:3]
+	c_info = country_info.loc[iso3]
+	(wet_season, dry_season) = get_wet_dry(c_info)
 
 
 	# PROCESSING
-    print(' ## NDVI pre/post implementation ##')
-    pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'NDVI')
-    print(' ## max NDVI pre/post implementation ##')
-    pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'maxNDVI')
-    print(' ## LST pre/post implementation ##')
-    pre_post.run(LST, shapefiles, wet_season, dry_season, asset_info, path_output, 'LST')
-    print(' ## Rainfall & max NDVI ##')
-    rfh_ndvi.run(CHIRPS, NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
-    print(' ## Expansion NDVI ##')
-    expansion_ndvi.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
-    print(' ## ENSO analysis ##')
-    enso.run(CHIRPS,shapefiles, wet_season, dry_season, ENSO, path_output, n_years=5)
+	if run[0] == 1:
+		print('\n' + ' ## NDVI pre/post implementation ##')
+		pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'NDVI')
+	if run[1] == 1:
+		print('\n' + ' ## max NDVI pre/post implementation ##')
+		pre_post.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output, 'maxNDVI')
+	if run[2] == 1:
+		print('\n' + ' ## LST pre/post implementation ##')
+		pre_post.run(LST, shapefiles, wet_season, dry_season, asset_info, path_output, 'LST')
+	if run[3] == 1:
+		print('\n' + ' ## TCI/VCI/VHI pre/post implementation ##')
+		tci_vci_vhi.run(LST, NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
+	if run[4] == 1:
+		print('\n' + '## Rainfall & max NDVI ##')
+		rfh_ndvi.run(CHIRPS, NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
+	if run[5] == 1:
+		print('\n' + ' ## Expansion NDVI ##')
+		expansion_ndvi.run(NDVI, shapefiles, wet_season, dry_season, asset_info, path_output)
+	if run[6] == 1:
+		print('\n' + ' ## ENSO analysis ##')
+		enso.run(CHIRPS,shapefiles, wet_season, dry_season, ENSO, path_output, n_years=5)
+
 
 
 if __name__ == '__main__':
 
-    main()
+	# Instantiate the parser
+	parser = argparse.ArgumentParser(description='LIA processing')
+
+	# Flags
+	parser.add_argument('--select', action='store_true', help='Select analysis')
+
+	# Parse
+	args = parser.parse_args()
+
+	main(args.select)
+
+
+
