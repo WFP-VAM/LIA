2b483fe0dc6215bbf8830d76b7a20a3b15124bd0
JuHolland <33092664+JuHolland@users.noreply.github.com>
2022-05-06 16:28:59 +0200

    added select flag

--- LIA_download.py
+++ LIA_download.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import pathlib
 import s3fs
+from tkinter import *
 import xarray as xr
 import zarr
 
@@ -9,17 +10,41 @@ from utils.helper_fns import delete_directory, ODC_to_disk, read_ODC
 
 
 
-def main(check_dates:bool):
+def main(check_dates:bool, select: bool):
+
+	# Checkbox
+	if select:
+
+		a = Tk()
+		a.title('Select')
+
+		positionRight = int(a.winfo_screenwidth()/2 - a.winfo_reqwidth()/2)
+		positionDown = int(a.winfo_screenheight()/2 - a.winfo_reqheight()/2)
+		a.geometry("+{}+{}".format(positionRight, positionDown))
+
+		var1 = IntVar()
+		Checkbutton(a, text = "NDVI.zarr", variable = var1).grid(row = 0, sticky = W)
+		var2 = IntVar()
+		Checkbutton(a, text = "LST.zarr", variable = var2).grid(row = 1, sticky = W)
+		var3 = IntVar()
+		Checkbutton(a, text = "CHIRPS.zarr", variable = var3).grid(row = 2, sticky = W)
+
+		Button(a, text = 'Download', command = a.destroy).grid(row = 3)
+		a.mainloop()
+
+		run = [var1.get(), var2.get(), var3.get()]
+
+	else:
+
+		run = [1, 1, 1]
 
 	# Set paths
 	path_to_ODC_url = 'data/Rasters/ODC_url.csv'
 	path_zarr = 'data/Rasters/zarr_data/'
 
-
 	# Rasters
 	ODC_url = pd.read_csv(path_to_ODC_url, index_col = 0, header = None)
-	ODC_url.rename(columns = {1: 'url'}, inplace = True)
-    
+	ODC_url.rename(columns = {1: 'url'}, inplace = True) 
     
     # Check dates of the datasets if --check_dates
 	if check_dates:
@@ -56,19 +81,22 @@ def main(check_dates:bool):
 		print('\n---------------------------------------------\n')
 
 		# NDVI
-		print('   --- Downloading NDVI Data ---')
-		name = path_zarr + 'NDVI.zarr'
-		ODC_to_disk(ODC_url.loc['NDVI']['url'], name)
+		if run[0] == 1:
+			print('   --- Downloading NDVI Data ---')
+			name = path_zarr + 'NDVI.zarr'
+			ODC_to_disk(ODC_url.loc['NDVI']['url'], name)
 
 		# LST
-		print('   --- Downloading LST Data ---')
-		name = path_zarr + 'LST.zarr'
-		ODC_to_disk(ODC_url.loc['LST']['url'], name)
+		if run[1] == 1:
+			print('   --- Downloading LST Data ---')
+			name = path_zarr + 'LST.zarr'
+			ODC_to_disk(ODC_url.loc['LST']['url'], name)
 
 		# CHIRPS
-		print('   --- Downloading CHIRPS Data ---')
-		name = path_zarr + 'CHIRPS.zarr'
-		ODC_to_disk(ODC_url.loc['CHIRPS']['url'], name)
+		if run[2] == 1:
+			print('   --- Downloading CHIRPS Data ---')
+			name = path_zarr + 'CHIRPS.zarr'
+			ODC_to_disk(ODC_url.loc['CHIRPS']['url'], name)
 
 		print('\n---------------------------------------------')
 
@@ -83,8 +111,9 @@ if __name__ == '__main__':
 
 	# Flags
 	parser.add_argument('--check_dates', action='store_true', help='Check available dates for each dataset')
+	parser.add_argument('--select', action='store_true', help='Select datasets to download')
 
 	# Parse
 	args = parser.parse_args()
 
-	main(args.check_dates)
+	main(args.check_dates, args.select)
